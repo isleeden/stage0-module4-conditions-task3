1d81956723064b5105305b154912fbe4626b3957
Denis Lee <51025257+isleeden@users.noreply.github.com>
2022-08-06 18:44:38 +0500

    Update Seasons.java

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-
+        if(month == 12 || month == 1 || month == 2) {
+            System.out.println("Winter");
+        }
+        if(3 <= month && month <= 5) {
+            System.out.println("Spring");
+        }
+        if(6 <= month && month <= 8) {
+            System.out.println("Summer");
+        }
+        if(9 <= month && month <= 11) {
+            System.out.println("Autumn");
+        } else {
+            System.out.println("wrong number!");
+        }        
     }
 }
