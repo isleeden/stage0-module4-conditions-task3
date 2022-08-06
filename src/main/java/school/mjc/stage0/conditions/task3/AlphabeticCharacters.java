0f23e5b9d2c2e605ce0240192457613d051ecebf
Denis Lee <51025257+isleeden@users.noreply.github.com>
2022-08-06 18:51:59 +0500

    Update AlphabeticCharacters.java

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,10 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
-        if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' ){
-            System.out.println("Vowel");
-        } else { 
-            System.out.println("Consonant");
+        int ch = symbol;
+        if((65 <= ch && ch <= 90) || (97 <= ch && ch <= 122)) { 
+            if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' || character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U'){
+                System.out.println("Vowel");
+            } else { 
+                System.out.println("Consonant");
+            }
+        } else {    
+            System.out.println("wrong alphabet!");
         }
+      
     }
 }
