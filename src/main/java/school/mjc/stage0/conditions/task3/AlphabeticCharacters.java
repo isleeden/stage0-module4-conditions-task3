2174401b9e9b5015981dfda34face85e05405d56
Denis Lee <51025257+isleeden@users.noreply.github.com>
2022-08-06 18:57:19 +0500

    Update AlphabeticCharacters.java

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
-        int ch = symbol;
+        int ch = character;
         if((65 <= ch && ch <= 90) || (97 <= ch && ch <= 122)) { 
             if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u'){
                 System.out.println("Vowel");
