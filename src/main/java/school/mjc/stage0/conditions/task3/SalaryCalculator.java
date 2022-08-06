afd7e7976a8bb623706afe26c17427cb9d506522
Denis Lee <51025257+isleeden@users.noreply.github.com>
2022-08-06 18:39:02 +0500

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if(0 < salary && salary <= 10000) {
+            System.out.println(salary * 0.85);
+        } else if (10000 < salary && salary <= 20000) {
+            System.out.println(salary * 0.82);
+        } else if (salary > 20000) {
+            System.out.println(salary * 0.80);
+        } else {
+            System.out.println("wrong input!");
+        }
     }
 }
